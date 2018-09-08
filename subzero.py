5dd34777435df3c42e4fec21c3793ebce0962ae3
Lim Wei Chiang <19551853+limweichiang@users.noreply.github.com>
2018-09-08 23:58:19 +0800

    Update subzero.py

--- subzero.py
+++ subzero.py
@@ -1,5 +1,5 @@
 
-# .  Copyright 2018 Lim Wei Chiang
+#   Copyright 2018 Lim Wei Chiang
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
